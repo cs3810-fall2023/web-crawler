35a458848654e25659a08ccddc840bac7d66b5f0
Hayden Blackmer <haydenblackmer@mail.weber.edu>
2023-10-30 20:26:06 -0600

    done

--- cmd/non-concurrent/findlinks.go
+++ cmd/non-concurrent/findlinks.go
@@ -9,27 +9,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"main/pkg/breadthFirst"
 	"main/pkg/links"
 	"os"
 )
-
-// !+breadthFirst
-// breadthFirst calls f for each item in the worklist.
-// Any items returned by f are added to the worklist.
-// f is called at most once for each item.
-func breadthFirst(f func(item string) []string, worklist []string) {
-	seen := make(map[string]bool)
-	for len(worklist) > 0 {
-		items := worklist
-		worklist = nil
-		for _, item := range items {
-			if !seen[item] {
-				seen[item] = true
-				worklist = append(worklist, f(item)...)
-			}
-		}
-	}
-}
 
 //!-breadthFirst
 
@@ -49,7 +32,7 @@ func crawl(url string) []string {
 func main() {
 	// Crawl the web breadth-first,
 	// starting from the command-line arguments.
-	breadthFirst(crawl, os.Args[1:])
+	breadthFirst.BreadthFirst(crawl, os.Args[1:])
 }
 
 //!-main
