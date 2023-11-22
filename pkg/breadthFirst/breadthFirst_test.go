31ae1864705b49010cdb8d552a0d14a46c17d930
Don B. Stringham <donbstringham@churchofjesuschrist.org>
2023-11-22 12:15:36 -0700

    Added a cspell.json file and another unit test.

--- pkg/breadthFirst/breadthFirst_test.go
+++ pkg/breadthFirst/breadthFirst_test.go
@@ -5,6 +5,45 @@ import (
 	"testing"
 )
 
+func TestBreadthFirst(t *testing.T) {
+    testCases := []struct {
+        name         string
+        f             func(item string) []string
+        worklist     []string
+        expectedSeen map[string]bool
+    }{
+        {
+            name:         "Simple case",
+            f:             func(item string) []string { return []string{"a", "b"} },
+            worklist:     []string{"c"},
+            expectedSeen: map[string]bool{"c": true, "a": true, "b": true},
+        },
+        {
+            name:         "Circular dependency",
+            f:             func(item string) []string { return []string{item} },
+            worklist:     []string{"a"},
+            expectedSeen: map[string]bool{"a": true},
+        },
+        {
+            name:         "Empty worklist",
+            f:             func(item string) []string { return []string{"a", "b"} },
+            worklist:     []string{},
+            expectedSeen: map[string]bool{},
+        },
+    }
+
+    for _, testCase := range testCases {
+        t.Run(testCase.name, func(t *testing.T) {
+            seen := make(map[string]bool)
+            BreadthFirst(testCase.f, testCase.worklist)
+
+            if !reflect.DeepEqual(seen, testCase.expectedSeen) {
+                t.Errorf("Expected seen: %v, got: %v", testCase.expectedSeen, seen)
+            }
+        })
+    }
+}
+/*
 var seen map[string]bool
 
 func TestBreadthFirst(t *testing.T) {
@@ -38,3 +77,4 @@ func TestBreadthFirst(t *testing.T) {
 		t.Errorf("Not all nodes were visited")
 	}
 }
+*/
